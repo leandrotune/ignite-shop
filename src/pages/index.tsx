3e75a233170192791857e164980cdd19a35452f4
leandro antunes <leandro.tune@hotmail.com>
2022-11-09 19:35:09 -0300

    Configurando o stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,21 @@
+import { styled } from "../styles"
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 8,
+  border: 0,
+  padding: 12,
+
+  span: {
+    fontWeight: 'bold',
+  }
+})
 
 export default function Home() {
   return (
-   <h1>Hello word</h1>
+   <Button>
+    <span>Teste</span>
+    Enviar
+   </Button>
   )
 }
