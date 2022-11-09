407ba4264c7e11ce62ce7c93c66097f2793e5b01
leandro antunes <leandro.tune@hotmail.com>
2022-11-09 19:56:27 -0300

    Estilos Globais

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,7 +12,17 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      rocketseat: '#8257e6',
+      white: '#FFF',
+
+      gray100: '#e1e1e6',
+      gray300: '#c4c4cc',
+      gray800: '#202024',
+      gray900: '#121214',
+
+      green300: '#00b37e',
+      green500: '#00875f',
+
+
     }
   }
 })
