51c4f6414c028a21162a254fd23a5ff35d3816a5
leandro antunes <leandro.tune@hotmail.com>
2022-11-11 20:28:43 -0300

    Estrutura da Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,31 @@
-import { styled } from "../styles"
+import Image from 'next/image'
 
-const Button = styled('button', {
-  backgroundColor: '$rocketseat',
-  borderRadius: 8,
-  border: 0,
-  padding: 12,
+import { HomeContainer, Product } from "../styles/pages/home"
 
-  span: {
-    fontWeight: 'bold',
-  }
-})
+import camiseta1 from '../assets/camiseta1.png'
+import camiseta2 from '../assets/camiseta2.png'
+import camiseta3 from '../assets/camiseta3.png'
 
 export default function Home() {
   return (
-   <Button>
-    <span>Teste</span>
-    Enviar
-   </Button>
+   <HomeContainer>
+    <Product>
+      <Image src={camiseta1} alt="" width={520} height={480}/>
+
+      <footer>
+        <strong>Camiseta</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+
+    <Product>
+      <Image src={camiseta1} alt="" width={520} height={480}/>
+
+      <footer>
+        <strong>Camiseta</strong>
+        <span>R$ 79,90</span>
+      </footer>
+    </Product>
+   </HomeContainer>
   )
 }
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -21,8 +21,13 @@ export const {
 
       green300: '#00b37e',
       green500: '#00875f',
+    },
 
-
-    }
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
   }
 })
