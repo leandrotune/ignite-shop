38091060243c0058373de0b010ceca8657583368
leandro antunes <leandro.tune@hotmail.com>
2023-01-25 18:39:32 -0300

    Navegação via Link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import { HomeContainer, Product } from "../styles/pages/home"
 import { useKeenSlider } from 'keen-slider/react'
 
 import 'keen-slider/keen-slider.min.css'
+import Link from 'next/link'
 
 interface HomeProps {
   products: {
@@ -31,14 +32,16 @@ export default function Home({ products }: HomeProps) {
    <HomeContainer ref={sliderRef} className="keen-slider">
     {products.map(product => {
       return ( 
-      <Product key={product.id} className='keen-slider__slide'>
-        <Image src={product.imageUrl} alt="" width={520} height={480}/>
+        <Link key={product.id} href={`/product/${product.id}`}>
+          <Product className='keen-slider__slide'>
+            <Image src={product.imageUrl} alt="" width={520} height={480}/>
 
-        <footer>
-          <strong>{product.name}</strong>
-          <span>{product.price}</span>
-        </footer>
-      </Product>
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       )
     })}
    </HomeContainer>
