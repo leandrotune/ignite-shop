29b4a15d1b9dabb4d196b1006fcf28beab2ff661
leandro antunes <leandro.tune@hotmail.com>
2023-01-25 18:04:32 -0300

    Formatando Preço

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,6 @@ import { HomeContainer, Product } from "../styles/pages/home"
 
 import { useKeenSlider } from 'keen-slider/react'
 
-
 import 'keen-slider/keen-slider.min.css'
 
 interface HomeProps {
@@ -31,13 +30,13 @@ export default function Home({ products }: HomeProps) {
   return (
    <HomeContainer ref={sliderRef} className="keen-slider">
     {products.map(product => {
-      return (
+      return ( 
       <Product key={product.id} className='keen-slider__slide'>
         <Image src={product.imageUrl} alt="" width={520} height={480}/>
 
         <footer>
           <strong>{product.name}</strong>
-          <span>R$ {product.price}</span>
+          <span>{product.price}</span>
         </footer>
       </Product>
       )
@@ -58,15 +57,16 @@ export const getStaticProps: GetStaticProps = async ({  }) => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount,
+      price:  new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+      }).format(price.unit_amount! / 100),
     }
   })
 
-  console.log(products)
-
   return {
     props: {
-     products
+     products,
     },
     revalidate: 60 * 60 * 2, // 2 hours
   }
