89cc31eab9bbe6269919dc158e96857621313a54
leandro antunes <leandro.tune@hotmail.com>
2023-01-26 21:45:46 -0300

    Estrutura da página de produto e estilização

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,23 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import { ImageContainer, ProductContainer, ProductDetails } from '../../styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product {JSON.stringify(query)} </h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 79,99</span>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos deserunt, ab assumenda ea, consequuntur recusandae quo aspernatur delectus mollitia ullam molestias quasi fuga. Inventore, fuga cupiditate sit corrupti saepe atque.</p>
+
+        <button>
+          comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
