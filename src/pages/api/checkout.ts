b96a9029ddf0047503e745351428d183ed77dac8
leandro antunes <leandro.tune@hotmail.com>
2023-03-08 21:49:36 -0300

    algumas alterações

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -4,19 +4,19 @@ import { stripe } from "../../lib/stripe";
 export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
   const { priceId } = req.body // passando O Id do Produto.
 
-  if(!priceId) {
-    return res.status(400).json({error: 'Price not found.'})
-  }
+  // if(!priceId) {
+  //   return res.status(400).json({error: 'Price not found.'})
+  // }
 
-  if(req.method !== 'POST') {
-    return res.status(405).json({error: 'Method not allowed.'})
-  }
+  // if(req.method !== 'POST') {
+  //   return res.status(405).json({error: 'Method not allowed.'})
+  // }
 
-  const sucessUrl = `${process.env.Next_URL}/success`
+  const successUrl = `${process.env.Next_URL}/success`
   const cancelUrl = `${process.env.Next_URL}/`
   
   const checkoutSession = await stripe.checkout.sessions.create({
-    success_url: sucessUrl,
+    success_url: successUrl,
     cancel_url: cancelUrl,
     mode: 'payment',
     line_items: [
--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { ImageContainer, SuccessContainer } from "../../styles/pages/success";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 
 
 export default function Success() {
